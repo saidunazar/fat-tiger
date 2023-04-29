84ec9b7a4ee0aa4bcd1bcaa2ef1b6cf9e159ca4e
saidunazar <saidunazar92@gmail.com>
2023-04-29 21:27:43 +0100

    Initial Commit

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const App = () => {
+  return <h4>Welcome to Fat Tiger!🐯</h4>;
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
